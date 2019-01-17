14390912b338b8fa1bfe23a880b38db5b441c2e9
Christian Roman <croman@themeetgroup.com>
2019-01-16 21:39:06 -0500

    v.1.6.0 Fixed a bug caused when the engine throws on startup.

--- bin/tail.js
+++ bin/tail.js
@@ -4,10 +4,11 @@ const RabbitTailEngine = require('../lib/engine');
 const Logger = require('../lib/log');
 const options = require('../lib/options');
 
-const engine = new RabbitTailEngine(options);
+const logger = new Logger(options.application);
+const engine = new RabbitTailEngine(options, logger);
 
 engine
   .start()
   .catch((err) => {
-    Logger.error(`Failed to start RabbitTail engine: ${err}`);
+    logger.error(`Failed to start RabbitTail engine: ${err}`);
   });
